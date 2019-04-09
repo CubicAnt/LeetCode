e467dc2319f4ab86adfd6dca2da6bf469b9f387f
CubicAnt <wy_ykt2015013066@163.com>
2019-04-09 12:18:51 +0800

    stack-normal-N331

---
+++ src/main/java/zjzs/cubicant/leetcodetraining/stack/normal/N331.java
@@ -0,0 +1,65 @@
+package zjzs.cubicant.leetcodetraining.stack.normal;
+
+import zjzs.cubicant.leetcodetraining.util.LeetCodeUtil;
+
+import java.util.LinkedList;
+
+public class N331 {
+    public static void main(String[] args) {
+        LeetCodeUtil.execute("#");
+        LeetCodeUtil.execute("#,#");
+        LeetCodeUtil.execute("#,4");
+        LeetCodeUtil.execute("#,#,#");
+        LeetCodeUtil.execute("9,3,4,#,#,1,#,#,2,#,6,#,#");
+        LeetCodeUtil.execute("1,#");
+        LeetCodeUtil.execute("9,#,#,1");
+        LeetCodeUtil.execute("9,3,4,#,#,1,#,#,#,2,#,6,#,#");
+    }
+
+    class Solution {
+        public boolean isValidSerialization(String preorder) {
+            String[] splits = preorder.split(",");
+            LinkedList<Node> stack = new LinkedList<>();
+            Node top;
+
+            if (splits.length == 1 && "#".equals(splits[0])) {
+                return true;
+            }
+
+            for (String split : splits) {
+                switch (split) {
+                    case "#":
+                        if (stack.isEmpty()) {
+                            return false;
+                        }
+
+                        while (!stack.isEmpty()) {
+                            top = stack.peek();
+                            if (top.leftVisited) {
+                                stack.pop();
+                            } else {
+                                top.leftVisited = true;
+                                break;
+                            }
+                        }
+                        break;
+                    default:
+                        stack.push(new Node(split));
+                        break;
+                }
+            }
+
+            return stack.isEmpty();
+        }
+
+        private class Node {
+            String value;
+            boolean leftVisited;
+
+            Node(String value) {
+                this.value = value;
+                this.leftVisited = false;
+            }
+        }
+    }
+}
